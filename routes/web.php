4c309458019405c5206688f9708948073139f7b4
jPee2k <kipishept@gmail.com>
2020-07-02 18:33:03 +0300

    moved '/about' to PageController

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', 'PageController@about');
