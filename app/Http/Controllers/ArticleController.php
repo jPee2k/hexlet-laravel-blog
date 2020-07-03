162d2d7161e85d7d1d50039fc927615d9649f4ed
jPee2k <kipishept@gmail.com>
2020-07-03 17:55:31 +0300

    article.show

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,8 +10,15 @@ class ArticleController extends Controller
 {   
     public function index()
     {
-        $articles = Article::paginate(2);
+        $articles = Article::paginate(10);
 
         return view('article.index', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+
+        return view('article.show', compact('article'));
     }
 }
